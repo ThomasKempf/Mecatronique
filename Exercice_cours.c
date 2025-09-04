b207d8dad20925c9861b8a029c486e9a00f4c6ae
Thomas Kempf-Driesbach <tkempfdriesbac01@pfm-ad.pfm.loc>
2025-09-04 16:05:09 +0200

    question 9

--- Exercice_cours.c
+++ Exercice_cours.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <stdint.h> // pour la definition de uint8_t
 
-int main()
-{
-    uint8_t a=0b00100110;
-    uint8_t b=0b01001011;
-    printf("a=%d, b=%d\n", a, b);
-    printf("%d, ", a>>1);
-    printf("%d, ", a>>2);
-    printf("%d, ", a&b);
-    printf("%d, ", a&0x0F);
-    printf("%d\n", (b&0x0F)<<1);
+int main(){
+    int n=10, p=5, q=10, r;
+    r = n == (p = q);
+    printf("A : n=%d, p=%d, q=%d, r=%d\n", n, p, q, r);
+    n = p = q = 5;
+    n += p += q;
+    printf("B : n=%d, p=%d, q=%d\n", n, p, q);
+    q = n < p ? n++ : p++;
+    printf("C : n=%d, p=%d, q=%d\n", n, p, q);
+    q = n > p ? n++ : p++;
+    printf("D : n=%d, p=%d, q=%d\n", n, p, q);
 }
